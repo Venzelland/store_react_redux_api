2a11501bad22ae8a363d19921e28fc7300d5d876
Venzelland <101016773+Venzelland@users.noreply.github.com>
2024-08-06 09:06:44 +0300

    updates Products.tsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
+import styles from '../../styles/Products.module.css';
 import {Link} from "react-router-dom";
 
-import styles from "../../styles/Products.module.css";
-
-const Products = ({title, style = {}, products = [], amount}) => {
+const Products = ({title, products = [], amount, style = {}}) => {
     const list = products.filter((_, i) => i < amount);
+
 
     return (
         <section className={styles.products} style={style}>
             {title && <h2>{title}</h2>}
 
             <div className={styles.list}>
-                {list.map(({id, images, title, category: {name: cat}, price}) => (
+                {list.map(({id, images, title, category: {name: cat}, price,}) => (
                     <Link to={`/products/${id}`} key={id} className={styles.product}>
                         <div
                             className={styles.image}
@@ -28,7 +28,6 @@ const Products = ({title, style = {}, products = [], amount}) => {
                                         {Math.floor(price * 0.8)}$
                                     </div>
                                 </div>
-
                                 <div className={styles.purchases}>
                                     {Math.floor(Math.random() * 20 + 1)} purchased
                                 </div>
@@ -38,7 +37,7 @@ const Products = ({title, style = {}, products = [], amount}) => {
                 ))}
             </div>
         </section>
-    );
+    )
 };
 
 export default Products;
